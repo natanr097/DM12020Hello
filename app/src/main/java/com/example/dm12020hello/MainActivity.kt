4739d88f7fcb9416a01dcace5413563b2c8ff7e5
natanr097 <natan12104@gmail.com>
2020-08-26 21:51:07 -0300

    limpando nome ao voltar para a tela principal

--- app/src/main/java/com/example/dm12020hello/MainActivity.kt
+++ app/src/main/java/com/example/dm12020hello/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.dm12020hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOk.setOnClickListener {
+
+            val nome = edtNome.text.toString()
+
+            val i = Intent(this, Tela2::class.java)
+
+            i.putExtra("nome_digitado", nome)
+
+            startActivity(i)
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        edtNome.text.clear()
+
     }
 }
---
+++ app/src/main/java/com/example/dm12020hello/tela2.kt
@@ -0,0 +1,18 @@
+package com.example.dm12020hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tela2.*
+
+class Tela2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela2)
+
+        val nome = intent.getStringExtra("nome_digitado")
+
+        txtBoasVindas.text = "Olá "+ nome + ", seja bem vindo!"
+
+    }
+}
